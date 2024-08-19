f892a89a8ab53e2d2410ee6685cab0f3cf5d5165
timmerj1 <timmerman.jeroen@gmail.com>
2024-08-20 00:23:24 +0200

    Changed and re-ran noC again

--- 01_simulation/noC_speed/noC.R
+++ 01_simulation/noC_speed/noC.R
@@ -4,14 +4,51 @@ library(EMC2)
 
 ADmat <- matrix(c(-1/2,1/2),ncol=1,dimnames=list(NULL,"d")); ADmat
 
-dat <-
-
-# MLBA ----
-
-designMLBA <- design(
+# Standard models ----
+# * LBA ====
+designLBA <- design(
   factors = list(S = c("left", "right"),
                  subjects = "Jeroen"),
   Rlevels = c("left", "right"),
+  model = LBA,
+  matchfun = function(d) d$S == d$lR,
+  formula = list(B ~ 1, v ~ lM, A ~ 1, sv ~ lM, t0 ~ 1),
+  constants = c(sv = 0),
+  contrasts=list(lM=ADmat)
+)
+
+parsLBA <- sampled_p_vector(designLBA,doMap = F)
+parsLBA[1:6] <- c(log(2),3,1,log(2),log(.75),log(.2))
+datLBA <- make_data(parsLBA, designLBA, n_trials = 10000)
+
+designRDM <- design(data=datLBA,model = RDM,
+                    matchfun = function(d) d$S == d$lR,
+                    formula = list(B ~ 1, v ~ lM, s ~ lM, t0 ~ 1),
+                    constants = c(s = 0),
+                    contrasts=list(lM=ADmat)
+)
+
+parsRDM <- sampled_p_vector(designRDM,doMap = F)
+parsRDM[1:5] <- c(log(3),1,.4,log(.75),log(.2))
+datRDM <- make_data(parsRDM, designRDM, n_trials = 10000)
+
+# * LNR ====
+designLNR <- design(data=datLBA,model = LNR,
+                    matchfun = function(d) d$S == d$lR,
+                    formula = list(m ~ lM, s ~ lM, t0 ~ 1),
+                    contrasts=list(lM=ADmat)
+)
+
+parsLNR <- sampled_p_vector(designLNR,doMap = F)
+parsLNR[1:5] <- c(log(.75),log(.65),log(.5),log(.8),log(.4))
+datLNR <- make_data(parsLNR, designLNR, n_trials = 10000)
+
+
+# Missing noC ----
+# * MLBA ====
+
+designMLBA <- design(
+  data = datLBA,
   model = MLBAnoC,
   matchfun = function(d) d$S == d$lR,
   formula = list(B ~ 1, v ~ lM, A ~ 1, sv ~ lM, t0 ~ 1, pContaminant ~ 1),
@@ -21,20 +58,16 @@ designMLBA <- design(
 
 parsMLBA <- sampled_p_vector(designMLBA,doMap = F)
 parsMLBA[1:6] <- c(log(2),3,1,log(2),log(.75),log(.2))
-datMLBA <- make_data(parsMLBA, designMLBA, n_trials = 10000)
-UC <- quantile(datMLBA$rt, 0.7)
-datMLBA$rt[datMLBA$rt > UC] <- Inf
-attr(datMLBA, "UC") <- unname(UC)
+UC <- quantile(datLBA$rt, 0.7)
+datMLBA <- make_data(parsMLBA, designMLBA, n_trials = 10000, UC = unname(UC))
 
 sMLBAnoC <- make_emc(datMLBA, designMLBA)
 save(sMLBAnoC,file="01_simulation/noC_speed/EMCs/sMLBAnoC.RData")
 
-# MRDM ----
+# * MRDM ====
 
 designMRDM <- design(
-  factors = list(S = c("left", "right"),
-                 subjects = "Jeroen"),
-  Rlevels = c("left", "right"),
+  data = datRDM,
   model = MRDMnoC,
   matchfun = function(d) d$S == d$lR,
   formula = list(B ~ 1, v ~ lM, s ~ lM, t0 ~ 1, pContaminant ~ 1),
@@ -44,20 +77,16 @@ designMRDM <- design(
 
 parsMRDM <- sampled_p_vector(designMRDM,doMap = F)
 parsMRDM[1:5] <- c(log(3),1,.4,log(.75),log(.2))
-datMRDM <- make_data(parsMRDM, designMRDM, n_trials = 10000)
-UC <- quantile(datMRDM$rt, 0.7)
-datMRDM$rt[datMRDM$rt > UC] <- Inf
-attr(datMRDM, "UC") <- unname(UC)
+UC <- quantile(datRDM$rt, 0.7)
+datMRDM <- make_data(parsMRDM, designMRDM, n_trials = 10000, UC = unname(UC))
 
 sMRDMnoC <- make_emc(datMRDM, designMRDM)
 save(sMRDMnoC,file="01_simulation/noC_speed/EMCs/sMRDMnoC.RData")
 
-# MLNR ----
+# MLNR ====
 
 designMLNR <- design(
-  factors = list(S = c("left", "right"),
-                 subjects = "Jeroen"),
-  Rlevels = c("left", "right"),
+  data = datLNR,
   model = MLNRnoC,
   matchfun = function(d) d$S == d$lR,
   formula = list(m ~ lM, s ~ lM, t0 ~ 1, pContaminant ~ 1),
@@ -67,10 +96,8 @@ designMLNR <- design(
 
 parsMLNR <- sampled_p_vector(designMLNR,doMap = F)
 parsMLNR[1:5] <- c(log(.75),log(.65),log(.5),log(.8),log(.4))
-datMLNR <- make_data(parsMLNR, designMLNR, n_trials = 10000)
-UC <- quantile(datMLNR$rt, 0.7)
-datMLNR$rt[datMLNR$rt > UC] <- Inf
-attr(datMLNR, "UC") <- unname(UC)
+UC <- quantile(datLNR$rt, 0.7)
+datMLNR <- make_data(parsMLNR, designMLNR, n_trials = 10000, UC = unname(UC))
 
 sMLNRnoC <- make_emc(datMLNR, designMLNR)
 save(sMLNRnoC,file="01_simulation/noC_speed/EMCs/sMLNRnoC.RData")
