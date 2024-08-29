40a7315e86269f136d6d09588dbb3d98efbe2b49
timmerj1 <timmerman.jeroen@gmail.com>
2024-08-29 11:17:55 +0200

    Prepared rerun noC

--- 01_simulation/noC_speed/run_files/runMLBAnoC.R
+++ 01_simulation/noC_speed/run_files/runMLBAnoC.R
@@ -1,4 +1,4 @@
 library(EMC2)
 load("./01_simulation/noC_speed/EMCs/sMLBAnoC.RData")
-sMLBAnoC <- fit(sMLBAnoC, fileName = "./01_simulation/noC_speed/run_files/tmpsMLBAnoC.RData")
+sMLBAnoC <- fit(sMLBAnoC, fileName = "./01_simulation/noC_speed/tmp_files/tmpsMLBAnoC.RData")
 save(sMLBAnoC, file = "./01_simulation/noC_speed/EMCs/sMLBAnoC.RData")
--- 01_simulation/noC_speed/run_files/runMLNRnoC.R
+++ 01_simulation/noC_speed/run_files/runMLNRnoC.R
@@ -1,4 +1,4 @@
 library(EMC2)
 load("./01_simulation/noC_speed/EMCs/sMLNRnoC.RData")
-sMLNRnoC <- fit(sMLNRnoC, fileName = "./01_simulation/noC_speed/run_files/tmpsMLNRnoC.RData")
+sMLNRnoC <- fit(sMLNRnoC, fileName = "./01_simulation/noC_speed/tmp_files/tmpsMLNRnoC.RData")
 save(sMLNRnoC, file = "./01_simulation/noC_speed/EMCs/sMLNRnoC.RData")
